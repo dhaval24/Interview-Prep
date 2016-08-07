25ac628decd03b0ba8a123fe2fa9e73d4cbcd7b9
dhaval24 <dhavaldoshi24@gmail.com>
2016-08-06 22:39:22 -0500

    Valid anagram problem

--- src/com/leetcode/hashtable/ValidAnagram.java
+++ src/com/leetcode/hashtable/ValidAnagram.java
@@ -2,6 +2,12 @@ package com.leetcode.hashtable;
 
 /**
  * Created by Dhaval on 7/29/2016.
+ * Given 2 strings find whether they are anagrams or not. Cat and Tac are anagrams
+ * Algorithm: Initialize a character array of length 256
+ * Iterate over string 1 and increase the value in array of the index corresponding to the ascii
+ * of character by 1
+ * Iterate through the second string, If the value at index ascii of character in array is 0 then
+ * not an anagram or else reduce the count in array by 1.
  */
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
@@ -12,7 +18,7 @@ public class ValidAnagram {
         int[] index = new int[256];
         for (int i = 0; i < len1; i++) {
             int current = s.charAt(i);
-            if (index[current] == 0) index[current] += 1;
+            index[current] += 1;
         }
         for (int i  = 0; i < len2; i++) {
             int current = t.charAt(i);
@@ -20,5 +26,9 @@ public class ValidAnagram {
             index[current] -= 1;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidAnagram().isAnagram("anagram", "nagaram"));
     }
 }
